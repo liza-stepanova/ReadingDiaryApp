d48cfa787405322ee703a14bfdc69fd5d17542fd
liza-stepanova <368852@niuitmo.ru>
2025-11-13 14:48:58 +0300

    Initial Commit

---
+++ ReadingDiaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadingDiaryApp
+//
+//  Created by Елизавета Степанова on 13.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
