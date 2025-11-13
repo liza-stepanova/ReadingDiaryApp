cacbb54d10bb4dc80664b35ca227f8d7859641f9
liza-stepanova <368852@niuitmo.ru>
2025-11-13 17:41:26 +0300

    проект подготовлен к вёрстке кодом

--- ReadingDiaryApp/ViewController.swift
+++ ReadingDiaryApp/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  ReadingDiaryApp
-//
-//  Created by Елизавета Степанова on 13.11.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
