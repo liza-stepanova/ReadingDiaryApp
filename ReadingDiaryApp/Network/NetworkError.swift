dc8678a14a71b14573613b16a7fa1f121255013e
liza-stepanova <368852@niuitmo.ru>
2025-11-17 21:29:58 +0300

    добавлен NetworkClient

---
+++ ReadingDiaryApp/Network/NetworkError.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+enum NetworkError: Error, LocalizedError {
+    
+    case emptyQuery
+    case invalidURL
+    case transport(Error)
+    case server(statusCode: Int)
+    case decoding(Error)
+
+    var errorDescription: String? {
+        switch self {
+        case .emptyQuery:
+            return "Пустой запрос"
+        case .invalidURL:
+            return "Неверный URL"
+        case .transport(let err):
+            return "Ошибка сети: \(err.localizedDescription)"
+        case .server(let code):
+            return "Ошибка сервера (\(code))"
+        case .decoding(let err):
+            return "Ошибка декодирования данных: \(err.localizedDescription)"
+        }
+    }
+    
+}
