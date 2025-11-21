5c1ee70487075c945c97516377681d872b5c140b
liza-stepanova <368852@niuitmo.ru>
2025-11-21 12:52:17 +0300

    добавлена view

---
+++ ReadingDiaryApp/Presentation/Screens/MyBooks/MyBooksContracts.swift
@@ -0,0 +1,22 @@
+protocol MyBooksPresenterProtocol: AnyObject {
+    
+    func itemViewModel(at index: Int) -> BookCellViewModel
+    func setViewController(view: FavoritesViewProtocol)
+    
+    var numberOfItems: Int { get }
+    func viewDidLoad()
+    func viewWillAppear()
+    
+    func didChangeStatus(for index: Int, to status: ReadingStatus)
+    func didToggleFavorite(for index: Int, isFavorite: Bool)
+    
+}
+
+protocol MyBooksViewProtocol {
+    
+    func reloadData()
+    func reloadItems(at indexes: [Int])
+    func showEmptyState(_ flag: Bool)
+    func showError(message: String)
+    
+}
